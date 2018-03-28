8964e2abf832f0bb7ff4aa36bcc67007efd518fc
kevinZV <35600973+kevinZV@users.noreply.github.com>
2018-03-28 22:26:23 +0200

    Add files via upload

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,18 +2,63 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BlogController extends Controller
 {
     /**
-     * @Route("/blog", name="blog")
+     * @Route("/", name="blog")
      */
     public function index()
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+        return new Response('
+            <html>
+                <body>
+                    <h1 style="text-align:center; margin-top:150px; font-family: sans-serif;">Welcome to your new controller!</h1>
+    
+    </body>
+        </html>');
+    }
+
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function blog(PostRepository $postRepository)
+    {
+        $posts = $postRepository->findAll();
+
+
+
+
+        return $this->render('blog.html.twig', [
+            'posts' => $posts,
         ]);
     }
+    /**
+     * @Route("/blog/post/new", name="new_blog_post")
+     */
+    public function newPost(Request $request)
+
+    {
+        $posts = new Post();
+
+        $form = $this->createForm(PostType::class, $posts);
+
+
+        return $this->render('news_blog_post.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/admin", name="admin")
+     */
+
+
 }
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Post;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +15,21 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('title', TextType::class, [
+                'label' => "Titre de l'atricle",
+            ])
+            ->add('body', TextareaType::class,
+                [
+                    'label' => "Contenu de l'articles",
+                ])
+
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
+            // uncomment if you want to bind to a class
             'data_class' => Post::class,
         ]);
     }
